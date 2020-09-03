b6807a6214086bf93fba9af5453d1f702af783fe
Yayan Artha <wayan.artha@nri.com>
2020-09-04 02:26:20 +0800

    class

---
+++ components/Classroom/ClassSideBar.tsx
@@ -0,0 +1,88 @@
+import PrimaryButton from '@components/common/PrimaryButton';
+import { useCallback } from 'react';
+
+type Props = {
+  handleBuyTicket(): void;
+};
+
+const ClassSideBar = (props: Props) => {
+  const _buyTicket = useCallback(() => {}, []);
+
+  return (
+    <div className="bg-white h-screen overflow-y-auto pt-12 relative" style={{ width: '450px' }}>
+      <div className="px-4 py-6 flex border-b border-gray-400">
+        <div className="flex flex-col items-center">
+          <div
+            className="flex items-center justify-center rounded-lg bg-primary"
+            style={{ width: '90px', height: '90px' }}
+          >
+            <p className="class-countdown">07</p>
+          </div>
+          <p className="class-countdown-label">DAY</p>
+        </div>
+
+        <div className="w-4" />
+
+        <div className="flex flex-col items-center">
+          <div
+            className="flex items-center justify-center rounded-lg bg-primary"
+            style={{ width: '90px', height: '90px' }}
+          >
+            <p className="class-countdown">11</p>
+          </div>
+          <p className="class-countdown-label">HOUR</p>
+        </div>
+
+        <div className="w-4" />
+
+        <div className="flex flex-col items-center">
+          <div
+            className="flex items-center justify-center rounded-lg bg-primary"
+            style={{ width: '90px', height: '90px' }}
+          >
+            <p className="class-countdown">25</p>
+          </div>
+          <p className="class-countdown-label">MINUTE</p>
+        </div>
+
+        <div className="w-4" />
+
+        <div className="flex flex-col items-center">
+          <div
+            className="flex items-center justify-center rounded-lg bg-primary"
+            style={{ width: '90px', height: '90px' }}
+          >
+            <p className="class-countdown">23</p>
+          </div>
+          <p className="class-countdown-label">SECOND</p>
+        </div>
+      </div>
+
+      <div className="px-4 py-6">
+        <p className="class-detail-title">Details</p>
+
+        <p className="class-detail-label">Information</p>
+        <p className="class-detail-value">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat.
+        </p>
+
+        <p className="class-detail-label">Hosted by</p>
+        <p className="class-detail-value">Nama Speaker</p>
+
+        <p className="class-detail-label">Duration</p>
+        <p className="class-detail-value">120 minutes</p>
+      </div>
+
+      <div className="absolute shadow-lg" style={{ bottom: '30px', left: 'calc(50% - 100px)' }}>
+        <PrimaryButton onClick={_buyTicket} label="Beli Tiket" width="200px" />
+      </div>
+    </div>
+  );
+};
+
+export default ClassSideBar;
--- pages/classroom.tsx
+++ pages/classroom.tsx
@@ -2,13 +2,20 @@ import Navbar from '@components/Navbar/Navbar';
 import Schedule from '@components/Schedule/Schedule';
 import { useState, useCallback } from 'react';
 import PrimaryButton from '@components/common/PrimaryButton';
+import ClassList from '@components/Classroom/ClassList';
+import ClassSideBar from '@components/Classroom/ClassSideBar';
 
 const Classroom = () => {
   const [isShowSchedule, setIsShowSchedule] = useState(true);
+  const [isShowSpeaker, setIsShowSpeaker] = useState(false);
 
   const _handleRegister = useCallback(() => {
     setIsShowSchedule(false);
   }, [isShowSchedule]);
+
+  const _handleBuyTicket = useCallback(() => {
+    setIsShowSpeaker(true);
+  }, [isShowSpeaker]);
 
   return (
     <div className="flex flex-col flex-1">
@@ -22,24 +29,33 @@ const Classroom = () => {
           objectFit: 'cover',
         }}
       >
-        <div
-          className="h-screen flex flex-col items-center overflow-y-auto"
-          style={{ backgroundColor: 'rgba(0,0,0,0.75)' }}
-        >
+        <div className="h-screen overflow-y-auto" style={{ backgroundColor: 'rgba(0,0,0,0.75)' }}>
           {isShowSchedule && (
-            <div
-              onClick={() => setIsShowSchedule(false)}
-              className="absolute w-screen h-screen flex flex-col items-center pt-24"
-            >
-              <Schedule title="Classroom Schedule" />
+            <div className="h-screen flex items-center justify-center">
+              <div
+                onClick={() => setIsShowSchedule(false)}
+                className="absolute w-screen h-screen flex flex-col items-center pt-24"
+              >
+                <Schedule title="Classroom Schedule" />
 
-              <div className="mt-6">
-                <PrimaryButton onClick={_handleRegister} label="Register Now" width="300px" />
+                <div className="mt-6">
+                  <PrimaryButton onClick={_handleRegister} label="Register Now" width="300px" />
+                </div>
+
+                <p className="schedule-footnote absolute" style={{ bottom: '60px' }}>
+                  Tekan tombol lonceng untuk mendapatkan notifikasi saat acara akan berlangsung
+                </p>
+              </div>
+            </div>
+          )}
+
+          {!isShowSchedule && !isShowSpeaker && (
+            <div className="flex">
+              <div className="flex flex-col flex-1 px-10 pt-24">
+                <ClassList />
               </div>
 
-              <p className="schedule-footnote absolute" style={{ bottom: '60px' }}>
-                Tekan tombol lonceng untuk mendapatkan notifikasi saat acara akan berlangsung
-              </p>
+              <ClassSideBar handleBuyTicket={_handleBuyTicket} />
             </div>
           )}
         </div>
