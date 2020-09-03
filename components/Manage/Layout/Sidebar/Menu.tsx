e76f81486d49305e30a4fab836e2151fc1f8c625
Agus Suarya <agus.suarya@spinroam.com>
2020-09-03 14:07:29 +0800

    Add hover action when sidebar menu is hovered.

--- components/Manage/Layout/Sidebar/Menu.tsx
+++ components/Manage/Layout/Sidebar/Menu.tsx
@@ -4,8 +4,24 @@ type Props = {
 
 const Menu = (props: Props) => {
   return (
-    <div className="h-12 text-white pl-8 flex items-center">
-      <span className="font-thin tracking-wider">{props.title}</span>
+    <div className="h-12 text-white pl-8 flex items-center menu-container">
+      <span className="font-thin tracking-wider noselect">{props.title}</span>
+
+      <style jsx>{`
+        .menu-container:hover {
+          background-color: #0470ad;
+          cursor: default;
+        }
+        .noselect {
+          -webkit-touch-callout: none; /* iOS Safari */
+          -webkit-user-select: none; /* Safari */
+          -khtml-user-select: none; /* Konqueror HTML */
+          -moz-user-select: none; /* Old versions of Firefox */
+          -ms-user-select: none; /* Internet Explorer/Edge */
+          user-select: none; /* Non-prefixed version, currently
+                                          supported by Chrome, Edge, Opera and Firefox */
+        }
+      `}</style>
     </div>
   );
 };
