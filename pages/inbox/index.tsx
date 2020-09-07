640ef85b45fa7cce483d17ae184e3b1fa7660c64
Reroet <wayanpuguhsudarma@gmail.com>
2020-09-07 23:16:37 +0800

    inbox

--- pages/inbox/index.tsx
+++ pages/inbox/index.tsx
@@ -2,22 +2,19 @@ import Header from '@components/Manage/Layout/Content/Header';
 import Tab from '@components/Manage/Layout/Content/NavigationTabs/Tab';
 import Tabs from '@components/Manage/Layout/Content/NavigationTabs/Tabs';
 import Layout from '@components/Manage/Layout/Layout';
+import Menu from '@components/Manage/Layout/Sidebar/Menu';
 
 const Index = () => {
   return (
     <Layout title="Inbox">
-      <Header title="Inbox" />
 
+      <Header title="Inbox" />
       <Tabs>
         <Tab title="General" isActive />
       </Tabs>
 
-      <div className="bg-white tab-content-container"></div>
-      <style jsx>{`
-        .tab-content-container {
-          min-height: 64vh;
-        }
-      `}</style>
+      <div className="bg-white shadow-md h-full"></div>
+
     </Layout>
   );
 };
