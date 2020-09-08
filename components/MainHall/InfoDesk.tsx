cd09315c515ca431f6d3d5aa0a59c109a3940acd
Yayan Artha <wayan.artha@nri.com>
2020-09-08 09:25:57 +0800

    benefit exhibitor visitor info desk

--- components/MainHall/InfoDesk.tsx
+++ components/MainHall/InfoDesk.tsx
@@ -1,7 +1,8 @@
 import { useState, useMemo, useCallback } from 'react';
 import TabHeader from '@components/common/TabHeader';
-import ImageViewerLayout from '@components/layout/ImageViewerLayout';
 import LargeModalContainer from '@components/common/LargeModalContainer';
+import ReactIcons from '@components/common/ReactIcons';
+import { FaCheck } from 'react-icons/fa';
 
 const InfoDesk = () => {
   const tabHeader = useMemo(
@@ -143,6 +144,62 @@ const InfoDesk = () => {
               </div>
             </div>
           </div>
+        </div>
+      )}
+
+      {activeTab === 1 && (
+        <div className="flex flex-col h-full w-3/5 overflow-y-auto px-8">
+          <p className="class-detail-label my-4">The Advantages of Virtual Expo for Exhibitors</p>
+          {[
+            'Involve more participants',
+            'Not limited to space and time',
+            'There are no queues when visiting',
+            'Design according to company branding',
+            'Realtime communication with booth owners',
+            'Can be accessed via tablet & smartphone',
+            'Professional tech support',
+            'IFRA as a Trusted platform to boost your brand image to the market',
+            'Get market update, trend and intelligence to grow your business',
+            'Expand your network and business contacts in the franchise and licensing industry',
+            'Opportunity to see and interact with more than a thousand potential business partners',
+          ].map((item, i) => (
+            <div className="flex">
+              <ReactIcons Icon={FaCheck} className="text-primary w-4 h-4 mr-4" />
+              <div className="mb-2">
+                <p className="brochure-desc">{item}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {activeTab === 2 && (
+        <div className="flex flex-col h-full w-3/5 overflow-y-auto px-8">
+          <p className="class-detail-label my-4">
+            5 Reason Why Must to Attend IFRA Virtual Expo for Visitors:
+          </p>
+          <p className="brochure-desc mb-4">
+            IFRA makes a valuable and effective tool for franchisor, licensor and producers to have
+            a direct interaction with potential franchisee, licensee, investor, entrepreneur and
+            business owner:
+          </p>
+
+          {[
+            'Visit dozens of virtual booths, making connections with key industry contacts.',
+            'Easy access & Flexible times for franchisee/licensee to see the profile of franchisor/licensor.',
+            'Schedule One-on-one between exhibitors and visitors via business matching.',
+            'Find the right business opportunity in every industry and investment level.',
+            'Learn from experts in virtual business presentation session.',
+          ].map((item, i) => (
+            <div className="flex">
+              <div className="w-6">
+                <p className="brochure-desc">{i + 1}.</p>
+              </div>
+              <div className="mb-2">
+                <p className="brochure-desc">{item}</p>
+              </div>
+            </div>
+          ))}
         </div>
       )}
     </LargeModalContainer>
