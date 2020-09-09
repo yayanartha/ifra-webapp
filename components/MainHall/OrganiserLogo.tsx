ad8ae31b1d9d8a3af7a8b64415ca48edd211b771
Yayan Artha <wayan.artha@nri.com>
2020-09-09 15:14:12 +0800

    main hall & exhibition

--- components/MainHall/OrganiserLogo.tsx
+++ components/MainHall/OrganiserLogo.tsx
@@ -2,29 +2,15 @@ type Props = {
   left: string;
   top: string;
   onClick: () => void;
-  label: string;
 };
 
 const OrganiserLogo = (props: Props) => {
   return (
-    <>
-      <div
-        onClick={props.onClick}
-        className="absolute rounded-full flex items-center justify-center cursor-pointer bg-white opacity-0 hover:opacity-25"
-        style={{ left: props.left, top: props.top, width: '8vw', height: '8vw' }}
-      />
-
-      <p
-        className="absolute text-white text-sm font-body font-bold text-center"
-        style={{
-          left: `calc(${props.left} - 1vw)`,
-          top: `calc(${props.top} - 3.5%)`,
-          width: '10vw',
-        }}
-      >
-        {props.label}
-      </p>
-    </>
+    <div
+      onClick={props.onClick}
+      className="absolute rounded-full flex items-center justify-center cursor-pointer bg-white opacity-0 hover:opacity-25"
+      style={{ left: props.left, top: props.top, width: '8vw', height: '8vw' }}
+    />
   );
 };
 
--- components/MainHall/OrganiserMiniLogo.tsx
+++ components/MainHall/OrganiserMiniLogo.tsx
@@ -2,28 +2,15 @@ type Props = {
   left: string;
   top: string;
   onClick(): void;
-  label: string;
 };
 
 const OrganiserMiniLogo = (props: Props) => {
   return (
-    <>
-      <div
-        onClick={props.onClick}
-        className="absolute rounded-full flex items-center justify-center cursor-pointer bg-white opacity-0 hover:opacity-25"
-        style={{ left: props.left, top: props.top, width: '8.3vw', height: '8.3vw' }}
-      />
-      <p
-        className="absolute text-text bg-white text-sm font-body font-bold text-center"
-        style={{
-          left: `calc(${props.left} - 1vw)`,
-          top: `calc(${props.top} - 2%)`,
-          width: '10vw',
-        }}
-      >
-        {props.label}
-      </p>
-    </>
+    <div
+      onClick={props.onClick}
+      className="absolute rounded-full flex items-center justify-center cursor-pointer bg-white opacity-0 hover:opacity-25"
+      style={{ left: props.left, top: props.top, width: '7.8vw', height: '7.8vw' }}
+    />
   );
 };
 
